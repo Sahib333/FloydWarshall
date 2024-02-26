09a6a4d01d920f1b9e3e399b1a233a81267064fa
Sahib333 <158838649+Sahib333@users.noreply.github.com>
2024-02-26 10:53:51 +0000

    Add files via upload

--- Week5_Assignment_FloydWarshall.py
+++ Week5_Assignment_FloydWarshall.py
@@ -3,14 +3,7 @@ import sys
 # Make pathsize max if no path exists
 INF = sys.maxsize
 
-#  Graph for testing 
-graph = [[0, 7, INF, 8],
-    [INF, 0, 5, INF],
-    [INF, INF, 0,   2],
-    [INF, INF, INF, 0],
-    ]
-# Automatically detect number of vertices
-vertex_number = len(graph)
+
 
 def floyd_warshall(graph):
 
@@ -19,9 +12,12 @@ def floyd_warshall(graph):
     by recursively looking at the shortest distance between each path    
     """
 
-    # Identify the dimensions of the graph
-  
+    # Automatically get dimensions of graph
+    # Declare as global to be used in subsequent function
+    global vertex_number
+    vertex_number = len(graph)
 
+    
     # Create an array with dimenions equal to the graph
     #  to update with the minimum path length
 
@@ -31,15 +27,18 @@ def floyd_warshall(graph):
     for i in range(vertex_number):
         dist[i]=graph[i]
 
-    print(dist)
     # Iterate over each row by using adding on the path distance
     # and determining if the path has shortened with the new node
      
     for k in range(vertex_number):
         for i in range(vertex_number):
             for j in range(vertex_number):
-                dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j])
-    
+                if dist[i][k]>=0 and dist[k][j]>=0:
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j])
+                elif (dist[i][k]<0 or dist[k][j]<0) and dist[i][k]!=INF and dist[k][j]!=INF:
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j])
+                else:
+                    min(dist[i][j],INF)    
     print_solution(dist)
     
 
@@ -47,6 +46,7 @@ def floyd_warshall(graph):
 # Create a matrix for the solution to be printed and replace the maxvalue
 #  with N for clarity and improving formatting of output
 def print_solution(dist):
+
     for i in range(vertex_number):
         for j in range(vertex_number):
             if(dist[i][j] == INF):
@@ -55,5 +55,26 @@ def print_solution(dist):
                 print(dist[i][j], end=" | ")
         print(" ")
 
+# Run the code for the desired graph
+if __name__=='__main__':
+    #  Graph for testing 
+    graph = [[0, 7, INF, 8],
+    [INF, 0, 5, INF],
+    [INF, INF, 0,   2],
+    [INF, INF, INF, 0]
+    ]
 
-floyd_warshall(graph)
+    """ 
+            7
+    (1)---------->(2)
+     |             |
+     |             | 5 
+   8 |             |
+     V             V
+    (4)<----------(3)
+            2
+    
+    """
+
+
+    floyd_warshall(graph)
